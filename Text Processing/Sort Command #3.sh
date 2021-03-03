e7634bea8ec71e006bfe752232a9927a078c3c20
Eran Levy <57839539+Eran1801@users.noreply.github.com>
2021-03-03 22:24:59 +0200

    Create Sort Command #3.sh

---
+++ Text Processing/Sort Command #3.sh
@@ -0,0 +1,8 @@
+sort -n
+
+:'
+-n Option : To sort a file numerically used –n option.
+-n option is also predefined in unix as the above options are.
+This option is used to sort the file with numeric data present inside.
+'
+
