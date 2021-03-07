379c54ea5e178776a2bb6109a32eebbc2842cca5
Eran Levy <57839539+Eran1801@users.noreply.github.com>
2021-03-07 16:00:23 +0200

    Create Paste - 1.sh

---
+++ Text Processing/Paste - 1.sh
@@ -0,0 +1,8 @@
+paste -d ";" -s
+
+: '
+
+-s (serial): We can merge the files in sequentially manner using the -s option. 
+It reads all the lines from a single file and merges all these lines into a single line with each line separated by tab
+
+'
